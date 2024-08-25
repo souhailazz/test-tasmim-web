675a36dcd832f6f42331c693695c2cd66b51a15c
souhail azzimani <101992394+souhailazz@users.noreply.github.com>
2024-08-25 19:45:39 +0100

    update

--- src/homepage/HomePage.jsx
+++ src/homepage/HomePage.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import './HomePage.css';
-import ServicesGrid from '../servicegrid/ServiceGrid.jsx'; // Correct import path
+import ServicesGrid from '../servicegrid/ServiceGrid.jsx';
+import GuideIcon from '../assets/icons/guide.svg';
+import PrestationsIcon from '../assets/icons/presentation.svg';
+import ProcessIcon from '../assets/icons/proc.svg';
+import TechnologiesIcon from '../assets/icons/technologies.svg';
+
+
 
 const HomePage = () => {
   return (
@@ -15,15 +21,25 @@ const HomePage = () => {
         </p>
         <button className="learn-more-button">En savoir plus</button><br/><br/><br/>
         <div className="nav-container">
-          <button className="nav-btn">Guides</button>
-          <button className="nav-btn">Nos prestations</button>
-          <button className="nav-btn">Notre processus</button>
-          <button className="nav-btn">Technologies</button>
+            <button className="nav-btn">
+              <img src={GuideIcon} alt="Guides Icon" />
+              Guides
+            </button>
+            <button className="nav-btn">
+              <img src={PrestationsIcon} alt="Prestations Icon" />
+              Nos prestations
+            </button>
+            <button className="nav-btn">
+              <img src={ProcessIcon} alt="Process Icon" />
+              Notre processus
+            </button>
+            <button className="nav-btn">
+              <img src={TechnologiesIcon} alt="Technologies Icon" />
+              Technologies
+            </button>
         </div>
       </header>
-      
-      {/* Add the ServicesGrid component here */}
-      <section className="services-section">
+            <section className="services-section">
         <ServicesGrid />
       </section>
     </div>
