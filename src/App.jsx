a939d3a0ffe96228a5ffcb216ffcbf14a3acf8e1
souhail azzimani <101992394+souhailazz@users.noreply.github.com>
2024-08-26 17:59:20 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css';
 import Header from './header/Header.jsx';
 import HomePage from './homepage/HomePage.jsx';
 
-
 function App() {
   return (
     <div className="App">
--- src/feature/Feature2.jsx
+++ src/feature/Feature2.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./features.css"; 
+import "./Feature.css"; 
 
 const Features = () => {
   return (
