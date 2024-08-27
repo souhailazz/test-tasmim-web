cb1f0eba0c66d8184522d49136e72280ac175a10
souhail azzimani <101992394+souhailazz@users.noreply.github.com>
2024-08-27 17:20:33 +0100

    update

--- src/homepage/HomePage.jsx
+++ src/homepage/HomePage.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './HomePage.css';
 import ServicesGrid from '../servicegrid/ServiceGrid.jsx'; 
 import Features from '../feature/Feature2.jsx';
 
 const HomePage = () => {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
     <div className="home-page">
       <header className="hero-section">
@@ -14,7 +35,11 @@ const HomePage = () => {
           productions dans plusieurs secteurs d'activités, têtus lorsqu'il <br/>
           s'agit d'accomplir une mission.
         </p>
-        <button className="learn-more-button">En savoir plus</button><br/><br/><br/>
+        <button className="learn-more-button">
+          <a href='https://www.tasmimweb.com/tasmim-web-agence-web-casablanca-maroc/' className="no-underline">
+            En savoir plus
+          </a>
+        </button><br/><br/><br/>
         <div className="nav-container">
           <button className="nav-btn">
             <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -48,14 +73,17 @@ const HomePage = () => {
       </section>
       
 
-      <div className="cta-container">
-        <h3 className="cta-heading">Prêt à digitaliser votre entreprise ?</h3>
-        <button className="learn-more-button">En savoir plus</button>
-      </div>
+<h3 className="cta-heading">Prêt à digitaliser votre entreprise ?</h3>
 
       <img className="imagesponsor" src="src/assets/images/sponsor.png" alt="Sponsor Image" />
 
       <Features />
+
+      {showButton && (
+        <button className="back-to-top" onClick={scrollToTop}>
+          <img src="src/assets/images/arrow-up.svg" alt="Retour en haut" />
+        </button>
+      )}
     </div>
   );
 };
