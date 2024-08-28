8141bba7fa96159359587f47faca68a382ebed8d
souhail azzimani <101992394+souhailazz@users.noreply.github.com>
2024-08-28 15:26:37 +0100

    c

---
+++ src/footer/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2023 TASMIM WEB. All rights reserved.</p>
+      <p>Rue 14 Amal 2 Imm 2, 3éme Etage N° 8, Sidi Bernoussi - Casablanca</p>
+      <p>Appelez-nous : +212 6 66 67 16 07</p>
+      <p>Email: contact@tasmimweb.com</p>
+    </footer>
+  );
+};
+
+export default Footer;
